13d7be97fbf29e4dced84074ae58332ae433b373
Angeldadro <148403077+Angeldadro@users.noreply.github.com>
2025-05-04 18:42:02 -0400

    feat: init shippings module

---
+++ src/contexts/shippings/domain/entitys/shipping.entity.ts
@@ -0,0 +1,88 @@
+import { OrderItem, OrderItemPrimitives } from "@orders/domain/entitys/order-item.entity";
+import { Address, AddressPrimitives } from "@shippings/domain/value-objects/address.vo";
+import { ShippingStatus, ShippingStatusEnum } from "@shippings/domain/value-objects/shipping-status.vo";
+
+interface ShippingPrimitives {
+  id: string;
+  orderId: string;
+  items: OrderItemPrimitives[];
+  shippingAddress: AddressPrimitives;
+  status: string;
+}
+
+export class Shipping {
+  private _id: string;
+  private _orderId: string;
+  private _items: OrderItem[];
+  private _shippingAddress: Address;
+  private _status: ShippingStatus;
+
+
+  constructor(
+    id: string,
+    orderId: string,
+    items: OrderItem[],
+    shippingAddress: Address,
+    status: ShippingStatus = new ShippingStatus(ShippingStatusEnum.PENDING),
+  ) {
+    this._id = id;
+    this._orderId = orderId;
+    this._items = items;
+    this._shippingAddress = shippingAddress;
+    this._status = status;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get orderId(): string {
+    return this._orderId;
+  }
+
+  get items(): OrderItem[] {
+    return this._items;
+  }
+
+  get shippingAddress(): Address {
+    return this._shippingAddress;
+  }
+
+  get status(): ShippingStatus {
+    return this._status;
+  }
+
+  markAsShipped(): void {
+    if (!this._status.isPending()) {
+      throw new Error(`Cannot ship in status: ${this._status.value}`);
+    }
+    this._status = new ShippingStatus(ShippingStatusEnum.SHIPPED);
+  }
+
+  markAsDelivered(): void {
+    if (!this._status.isShipped()) {
+      throw new Error(`Cannot mark as delivered in status: ${this._status.value}`);
+    }
+    this._status = new ShippingStatus(ShippingStatusEnum.DELIVERED);
+  }
+
+  static fromPrimitives(data: ShippingPrimitives): Shipping {
+    return new Shipping(
+      data.id,
+      data.orderId,
+      data.items.map((item) => OrderItem.fromPrimitives(item)),
+      Address.fromPrimitives(data.shippingAddress),
+      ShippingStatus.fromString(data.status),
+    );
+  }
+
+  toPrimitives(): ShippingPrimitives {
+    return {
+      id: this._id,
+      orderId: this._orderId,
+      items: this._items.map((item) => item.toPrimitives()),
+      shippingAddress: this._shippingAddress.toPrimitives(),
+      status: this._status.value,
+    };
+  }
+} 
