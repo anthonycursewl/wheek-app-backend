c81cb71c86b7bf4cce1aeaf554d352806947031b
angel.delgado <148403077+Angeldadro@users.noreply.github.com>
2025-05-05 03:28:46 -0400

    fix: k8s bad gateway error

--- src/main.ts
+++ src/main.ts
@@ -31,12 +31,12 @@ async function bootstrap() {
     .setDescription('Descripción de la API creada con NestJS, Bun y Fastify')
     .setVersion('1.0')
     .addBearerAuth()
-    
+
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
 bootstrap();
