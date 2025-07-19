3e3245c9cb99b0f3683e18433a8fb78c245895df
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-07-18 23:41:22 -0400

    feat: Implement providers controller with CRUD operations

---
+++ src/contexts/products/infrastructure/controllers/providers.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Post, UsePipes, ValidationPipe, Body, Get, Query } from "@nestjs/common";
+import { CreateProviderUseCase } from "../../application/providers/create-provider.usecase";
+import { CreateProviderDto } from "../dtos/providers/create-provider.dto";
+import { GetAllProvidersUseCase } from "../../application/providers/get-all-providers.usecase";
+
+@Controller('providers')
+export class ProvidersController {
+    constructor(
+        private readonly createProviderUseCase: CreateProviderUseCase,
+        private readonly getAllProvidersUseCase: GetAllProvidersUseCase
+    ) {}
+
+    @Post('create')
+    @UsePipes(new ValidationPipe({ transform: true }))
+    async create(@Body() provider: CreateProviderDto) {
+        return this.createProviderUseCase.execute(provider);
+    }
+
+    @Get('all')
+    async getAllProviders(@Query() query: { store_id: string, skip: string, take: string }) {
+        if (!query.store_id) throw new Error('Store ID is required')
+        if (!query.skip || !query.take) query.skip = '0'; query.take = '10'
+
+        return this.getAllProvidersUseCase.execute(query.store_id, parseInt(query.skip), parseInt(query.take))
+    }
+}
