8db174d04edad2111fc1499d3c4ba0ded43a077e
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-07-18 23:41:12 -0400

    📘 feat: Implement categories controller for product context

---
+++ src/contexts/products/infrastructure/controllers/categories.controller.ts
@@ -0,0 +1,24 @@
+import { Post, Body, UsePipes, ValidationPipe, Controller, Get, Query, Param } from "@nestjs/common";
+import { CreateCategoryUseCase } from "../../application/categories/create-category.usecase";
+import { CreateCategoryDto } from "../dtos/categories/create-category.dto";
+import { GetAllCategoriesUseCase } from "../../application/categories/get-all-categories.usecase";
+
+@Controller('categories')
+export class CategoryController {
+    constructor(
+        private readonly createCategoryUseCase: CreateCategoryUseCase,
+        private readonly getAllCategoriesUseCase: GetAllCategoriesUseCase
+    ) {}
+
+    @Post('create')
+    @UsePipes(new ValidationPipe({ transform: true }))
+    async create(@Body() category: CreateCategoryDto) {
+        return await this.createCategoryUseCase.execute(category);
+    }
+
+    @Get('all/:store_id')
+    async getAllCategoriesByStoreId(@Query() query: { skip: string, take: string }, @Param('store_id') store_id: string) {
+        if (!query.skip || !query.take) query.skip = '0'; query.take = '10'
+        return await this.getAllCategoriesUseCase.execute(store_id, Number(query.skip), Number(query.take));
+    }
+}
