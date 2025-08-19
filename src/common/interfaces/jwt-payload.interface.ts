9ee156771e08ff8ae56b0d2ba07e65cbf00f8cc7
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-08-19 00:55:00 -0400

    feat: Add role to JWT payload interface

--- src/common/interfaces/jwt-payload.interface.ts
+++ src/common/interfaces/jwt-payload.interface.ts
@@ -1,6 +1,10 @@
+import { Role } from '../enums/roles.enum';
+
 export interface JwtPayload {
   sub: string;
   email: string;
+  role: Role;
+  // Add any additional user properties you want to include in the JWT token
 }
 
 export interface RequestWithUser extends Request {
